c3ecc6570bdfbaf251b75ce3a8222c8c372f25b9
Joseph <j.moorhouse1898@gmail.com>
2020-04-07 16:55:17 +0100

    Added data formatting methods

--- sales_analysis/data_pipeline/_pipeline.py
+++ sales_analysis/data_pipeline/_pipeline.py
@@ -1,6 +1,7 @@
-from flask import Flask
-import datetime as datetime
 import pandas as pd 
+
+# --------------------------------------------------------------------------
+# Data pipeline
 
 class SalesPipeline:
     # ----------------------------------------------------------------------
@@ -10,4 +11,21 @@ class SalesPipeline:
         for k, v in data.items():
             setattr(self, k, v)
             
+         self.merged_order_data = self._merge_order_data()
 
+    # ----------------------------------------------------------------------
+    # formatting methods 
+            
+    def _format_orders(self, orders):
+        orders["created_at"] = pd.to_datetime(orders["created_at"]).dt.date
+        return orders
+    
+    def _merge_order_data(self):
+        orders = self._format_orders(self._orders)
+        order_lines = self._order_lines
+        
+        return order_lines.merge(
+            orders, 
+            how="left", 
+            left_on="order_id", 
+            right_on="id",
