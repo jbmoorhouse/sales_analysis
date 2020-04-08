d8fb8f390561c7617dc0f7a0497919002e5f3623
Joseph <j.moorhouse1898@gmail.com>
2020-04-08 15:43:31 +0100

    Changed version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 def setup_package():
     setup(
         name='sales-analysis',
-        version='0.1',
+        version='0.2',
         author='Joseph Moorhouse',
         license=license,
         description='A sales analysis package for an imaginary e-shop',
